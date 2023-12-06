25d32f2283c96e19bd402f09e383f95c558d380b
Eric <eric@jaholl.de>
2023-12-06 22:05:45 +0100

    Initial commit

---
+++ src/main/java/de/imbadingerman/foodadder/Foodadder.java
@@ -0,0 +1,17 @@
+package de.imbadingerman.foodadder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Foodadder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
