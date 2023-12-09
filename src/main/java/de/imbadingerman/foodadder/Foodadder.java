6b96d09bc10e38b87b48e7763dcfb7b5adbcc445
Eric <eric@jaholl.de>
2023-12-09 11:13:11 +0100

    messages.yml added

--- src/main/java/de/imbadingerman/foodadder/Foodadder.java
+++ src/main/java/de/imbadingerman/foodadder/Foodadder.java
@@ -5,32 +5,34 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Foodadder extends JavaPlugin {
-
-    private FileConfiguration messagesConfig;
-
     @Override
     public void onEnable() {
-        loadMessagesConfig();
-
-        String pluginEnabledMessage = messagesConfig.getString("plugin_enabled");
-        getLogger().info(pluginEnabledMessage);
-    }
-
-    @Override
-    public void onDisable() {
-        String pluginDisabledMessage = messagesConfig.getString("plugin_disabled");
-        getLogger().info(pluginDisabledMessage);
-    }
-
-    private void loadMessagesConfig() {
         File messagesFile = new File(getDataFolder(), "messages.yml");
-
         if (!messagesFile.exists()) {
             saveResource("messages.yml", false);
         }
 
-        messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
+        FileConfiguration messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
+
+        String message = messagesConfig.getString("plugin_enabled");
+
+        getLogger().info(message);
+        Objects.requireNonNull(getCommand("fooditems")).setExecutor(new FoodCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        File messagesFile = new File(getDataFolder(), "messages.yml");
+        if (!messagesFile.exists()) {
+            saveResource("messages.yml", false);
+        }
+
+        FileConfiguration messagesConfig = YamlConfiguration.loadConfiguration(messagesFile);
+    String disable_message = messagesConfig.getString("plugin_disabled");
+
+    getLogger().info(disable_message);
     }
 }
